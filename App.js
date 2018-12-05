2cd35eb07230ecb450cc6f5c5b0039248f7de305
Chen <chenyu.zhu86@gmail.com>
2018-12-04 23:01:06 -0500

    added firebase boilerplate to project

--- App.js
+++ App.js
@@ -3,8 +3,21 @@ import { Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './src/reducers'
+import firebase from 'firebase'
 
 class App extends React.Component {
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyCOlDrRWHY_d_6FDVe7fk2vfFPYCQ3bAPE",
+      authDomain: "manager-30cba.firebaseapp.com",
+      databaseURL: "https://manager-30cba.firebaseio.com",
+      projectId: "manager-30cba",
+      storageBucket: "manager-30cba.appspot.com",
+      messagingSenderId: "167587029322"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}> 
