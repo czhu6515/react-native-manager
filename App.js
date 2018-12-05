0ad42ddb1b566acffae8472aea545251eb51ade1
Chen <chenyu.zhu86@gmail.com>
2018-12-04 22:53:47 -0500

    created dummy reducer to get app rolling

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducers from './src/reducers'
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore()}> 
+      <Provider store={createStore(reducers)}> 
         <View>
           <Text>Open up App.js to start working on your app!</Text>
         </View>
