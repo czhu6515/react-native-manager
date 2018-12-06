934a728d2cb2876a117c6980728e3d8227396ec1
Chen <chenyu.zhu86@gmail.com>
2018-12-06 13:41:01 -0500

    created EmployeeList component and imported into router

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LogInForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
   return(
     <Router>
       <Scene key='root'>
-        <Scene key='login' component={LoginForm} title='Please Login' />
+        <Scene 
+          key='login' 
+          component={LoginForm} 
+          title='Please Login' 
+          initial
+        />
+        <Scene 
+          key='employee_list'
+          component={EmployeeList}
+          title='Employees'
+        />
       </Scene>
     </Router>
   )
