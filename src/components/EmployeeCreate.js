5453e08f60f864da8477bd1fbeed12a33a8fe0e0
Chen <chenyu.zhu86@gmail.com>
2018-12-06 16:33:18 -0500

    created scaffolding for redux to handle employee form state changes

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Card, CardSection, Button, Input} from './common/index'
 
 class EmployeeCreate extends Component {
   render(){
     return(
-      <View>
-        <Text>From emp create!</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label='Name' placeholder='Bob' />
+        </CardSection>
+
+        <CardSection>
+          <Input label='Phone' placeholder='555-555-5555' />
+        </CardSection>
+
+        <CardSection>
+
+        </CardSection>
+        
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     )
   }
 }
